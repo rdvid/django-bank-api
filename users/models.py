d87889deb4740a59faa4cc36afe72e05c5667154
Rafael <rafael_dvid@hotmail.com>
2024-02-20 23:15:22 -0300

    refactoring constraints and serializer field settings

--- users/models.py
+++ users/models.py
@@ -1,29 +1,12 @@
-from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-
-
-class BaseModel(models.Model):
-    # TODO: {created/updated}_by
-    # created_by = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
-    # updated_by = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    created_at = models.DateTimeField(blank=False, default=datetime.now(), editable=False)
-    updated_at = models.DateTimeField(blank=False, default=datetime.now(), editable=False)
-
-    class Meta:
-        abstract=True
+from setup.models import BaseModel
 
 
 class UserModel(BaseModel):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
-    username = models.CharField(max_length=100, unique=True)
-
-    class Meta:
-        ordering = ('username',)
-        app_label = 'users'
 
     def __str__(self):
-        return self.username
+        return f'{self.user.username}'
--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-
+from setup.serializers import BaseSerializer
 from users.models import UserModel
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(BaseSerializer):
     user = serializers.RelatedField(
         many=False,
         read_only=True,
